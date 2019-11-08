ddbb0694a59dc09cbf25b4855a07a466809493e2
Nutthanich Narphromar <monaprom134@gmail.com>
2019-11-08 23:14:31 +0700

    Replace user's previous vote with new vote

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -25,3 +26,11 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class Vote(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # TODO: how to add a uniqueness constraint on database so there is only one vote per (User, Question) pair.
